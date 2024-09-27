78cc7d14884b6465c1b9593a59daea8f9438c77c
athom1662 <thom1662@algonquinlive.com>
2024-09-26 20:05:54 -0400

    init; names displayed in list

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import UserAvatar from 'react-native-user-avatar';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+const [users, setUsers] = useState([])
+
+useEffect( () => {
+  axios.get('https://random-data-api.com/api/v2/users?size=10')
+  .then( response => {
+    setUsers(response.data)
+  })
+  .catch( error => {
+    console.error('error fetching users: ', error);
+  });
+}, []);
+
+
+const listItem = ({item}) => (
+  <View style={styles.name}>
+    <Text>{item.first_name}</Text>
+    <Text>{item.last_name}</Text>
+  </View>
+);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+
+        <FlatList
+        data={users}
+        renderItem={listItem}
+        keyExtractor={item => item.id.toString()}
+
+        >
+        </FlatList>
+
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +52,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name: {
+    padding: 10,
+  }
 });
